d527c9a7780d737c3bebac239d23041e674b1751
galang <galangrambuanarki.170320@gmail.com>
2025-08-15 18:54:42 +0700

    adding loading

--- News/src/pages/home.tsx
+++ News/src/pages/home.tsx
@@ -2,18 +2,36 @@ import { useEffect,useState } from "react"
 import { fetchTopStories } from "../library/api"
 import NewsList from "../components/newlist"
 import SearchBar from "../components/searchbar"
+import Swal from "sweetalert2";
 
 export default function Home(){
     const[articles,setArticles]=useState<any[]>([]);
     const[loading,setLoading]=useState(true)
 
     useEffect(()=>{
-        fetchTopStories().then(data=>{
-            setArticles(data);
-            setLoading(false);
-        })
+        Swal.fire({
+            title: "Loading latest news...",
+            text: "Please wait while we fetch the articles",
+            allowOutsideClick: false,
+            didOpen: () => {
+                Swal.showLoading();
+            }
+        });
+         fetchTopStories()
+            .then(data => {
+                setArticles(Array.isArray(data) ? data : []);
+            })
+            .catch(err => {
+                console.error(err);
+                Swal.fire("Error", "Failed to fetch news data", "error");
+            })
+            .finally(() => {
+                setLoading(false);
+                Swal.close();
+            });
     },[]
 );
+if (loading) return null;
     return (
         <div className="p-4">
             <h1 className="text-2xl font-bold uppercase">
