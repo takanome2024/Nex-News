9476f6aa40298561adcb0960038301d8df798bef
galang <galangrambuanarki.170320@gmail.com>
2025-08-15 18:49:09 +0700

    recommit: vercel error

--- News/src/main.tsx
+++ News/src/main.tsx
@@ -1,5 +1,4 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
